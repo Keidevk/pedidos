02c55f9898057a10f2281824d5001cef21a4bddd
Alvaro <mitsuyaok@gmail.com>
2025-08-05 00:07:27 -0400

    refining sellerDashboardProducts

--- app/(seller)/sellerDashboardProducts.tsx
+++ app/(seller)/sellerDashboardProducts.tsx
@@ -1,3 +1,10 @@
+import {
+  Inter_300Light,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  useFonts,
+} from "@expo-google-fonts/inter";
 import { useEffect, useState } from "react";
 import { ActivityIndicator, FlatList, Image, Text, View } from "react-native";
 import { getItem } from "../utils";
@@ -12,6 +19,12 @@ type Product = {
 };
 
 export default function SellerDashboardProducts() {
+  useFonts({
+    Inter_600SemiBold,
+    Inter_300Light,
+    Inter_400Regular,
+    Inter_700Bold,
+  });
   const [products, setProducts] = useState<Product[]>([]);
   const [loading, setLoading] = useState(true);
   const [userId, setUserId] = useState<string>("");
@@ -40,57 +53,86 @@ export default function SellerDashboardProducts() {
   if (loading) return <ActivityIndicator size="large" color="#D61355" />;
 
   return (
-    <FlatList
-      data={products}
-      keyExtractor={(item) => item.id.toString()}
-      contentContainerStyle={{ padding: 12 }}
-      renderItem={({ item }) => (
-        <View
-          style={{
-            flexDirection: "row",
-            marginBottom: 12,
-            backgroundColor: "#fff",
-            borderRadius: 8,
-            overflow: "hidden",
-            elevation: 2,
-          }}
-        >
-          {item.imagen_url ? (
-            <Image
-              source={{ uri: item.imagen_url }}
-              style={{ width: 80, height: 80, backgroundColor: "#ccc" }}
-            />
-          ) : (
-            <View
-              style={{
-                width: 80,
-                height: 80,
-                backgroundColor: "#ccc",
-                justifyContent: "center",
-                alignItems: "center",
-              }}
-            >
-              <Text>No imagen</Text>
+    <View
+      style={{
+        padding: 20,
+        gap: 20,
+      }}
+    >
+      <View style={{ width: "100%" }}>
+        <Text style={{ fontFamily: "Inter_700Bold" }}>Mis productos</Text>
+      </View>
+      <FlatList
+        data={products}
+        keyExtractor={(item) => item.id.toString()}
+        contentContainerStyle={{ padding: 12, gap: 10 }}
+        renderItem={({ item }) => (
+          <View
+            style={{
+              flexDirection: "row",
+              marginBottom: 12,
+              backgroundColor: "#fff",
+              borderRadius: 8,
+              overflow: "hidden",
+              elevation: 2,
+            }}
+          >
+            {item.imagen_url ? (
+              <Image
+                source={{ uri: item.imagen_url }}
+                style={{ width: 80, height: 80, backgroundColor: "#ccc" }}
+              />
+            ) : (
+              <View
+                style={{
+                  width: 80,
+                  height: 80,
+                  backgroundColor: "#ccc",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <Text
+                  style={{
+                    fontFamily: "Inter_600SemiBold",
+                  }}
+                >
+                  No imagen
+                </Text>
+              </View>
+            )}
+            <View style={{ flex: 1, padding: 8 }}>
+              <Text
+                style={{
+                  fontWeight: "bold",
+                  fontSize: 16,
+                  fontFamily: "Inter_600SemiBold",
+                }}
+              >
+                {item.nombre}
+              </Text>
+              <Text>{item.descripcion}</Text>
+              <Text
+                style={{
+                  color: "#D61355",
+                  fontWeight: "bold",
+                  marginTop: 4,
+                  fontFamily: "Inter_300Light",
+                }}
+              >
+                ${item.precio}
+              </Text>
+              <Text
+                style={{
+                  fontFamily: "Inter_300Light",
+                }}
+              >
+                Stock: {item.stock_actual}
+              </Text>
             </View>
-          )}
-          <View style={{ flex: 1, padding: 8 }}>
-            <Text style={{ fontWeight: "bold", fontSize: 16 }}>
-              {item.nombre}
-            </Text>
-            <Text>{item.descripcion}</Text>
-            <Text
-              style={{
-                color: "#D61355",
-                fontWeight: "bold",
-                marginTop: 4,
-              }}
-            >
-              ${item.precio}
-            </Text>
-            <Text>Stock: {item.stock_actual}</Text>
           </View>
-        </View>
-      )}
-    />
+        )}
+      />
+    </View>
   );
 }
